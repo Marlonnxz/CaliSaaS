5bcb69a791b5cbbb29d746ea01929860ea5d7023
Marlonnxz <marlondelgado1998@gmail.com>
2026-05-03 18:47:34 -0500

    fix: IP dinámica en frontend para desarrollo en equipo

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     apiUrlNode: 'http://192.168.1.5:3000/api',
     keycloakUrl: 'http://192.168.1.5:8082',
     keycloakRealm: 'CaliSaaS',
-    keycloakClientId: 'angular-frontend',
+    keycloakClientId: 'frontend-react',
     theme: {
       primaryColor: '#ff4b4b',
       secondaryColor: '#2d3436',
